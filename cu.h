8669492acad973ab0ebf74dae4804868c05db14d
Holger Langenau <holger.langenau@mathematik.tu-chemnitz.de>
2018-09-24 15:08:54 +0200

    Fix round to 8/16.

--- cu.h
+++ cu.h
@@ -13,8 +13,8 @@
 #define CU_UINT_TO_POINTER(u) ((void *)(uint64_t)(uint32_t)(u))
 #endif
 
-/* FIXME: Does this work if we are already a multiple of 16? */
-#define ROUND_TO_16(n) ((((n) >> 4) + 1) << 4)
+#define ROUND_TO_8(n)  ((((n) + 7)  >> 3) << 3)
+#define ROUND_TO_16(n) ((((n) + 15) >> 4) << 4)
 
 #define cu_likely(x)   __builtin_expect((x), 1)
 #define cu_unlikely(x) __builtin_expect((x), 0)
