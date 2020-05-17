f20e883b668f5f7b886ef69833dbde1ba19c4433
Holger Langenau <holger.langenau@mathematik.tu-chemnitz.de>
2020-05-17 09:54:24 +0200

    More benchmarking of fixed alloc/free.

--- bm-fixed-mem.c
+++ bm-fixed-mem.c
@@ -17,27 +17,30 @@ int main(int argc, char **argv)
     clock_t now;
     CUFixedSizeMemoryPool *pool = cu_fixed_size_memory_pool_new(element_size, group_size);
     cu_fixed_size_memory_pool_release_empty_groups(pool, true);
-    now = clock();
-    fprintf(stdout, "pool(%zu, %zu) new: %fs, alloc %" PRIu64 "\n",
-            element_size, group_size, (double)(now - starttime)/CLOCKS_PER_SEC, alloc_count);
-    starttime = now;
-    CUList *list = NULL;
-    for (j = 0; j < alloc_count; ++j) {
-        list = cu_list_prepend(list, cu_fixed_size_memory_pool_alloc(pool));
+
+    uint32_t k;
+    for (k = 0; k < 2; ++k) {
+        now = clock();
+        fprintf(stdout, "pool(%zu, %zu) new: %fs, alloc %" PRIu64 "\n",
+                element_size, group_size, (double)(now - starttime)/CLOCKS_PER_SEC, alloc_count);
+        starttime = now;
+        CUList *list = NULL;
+        for (j = 0; j < alloc_count; ++j) {
+            list = cu_list_prepend(list, cu_fixed_size_memory_pool_alloc(pool));
+        }
+        now = clock();
+        fprintf(stdout, "alloc %" PRIu64 ": %fs\n", alloc_count, (double)(now - starttime)/CLOCKS_PER_SEC);
+        starttime = now;
+        j = 0;
+        while (list) {
+            cu_fixed_size_memory_pool_free(pool, list->data);
+            list = cu_list_delete_link(list, list);
+    /*        if (++j == alloc_count/2)
+                cu_fixed_size_memory_pool_release_empty_groups(pool, true);*/
+        }
+        now = clock();
+        fprintf(stdout, "free %" PRIu64 ": %fs\n", alloc_count, (double)(now - starttime)/CLOCKS_PER_SEC);
     }
-    now = clock();
-    fprintf(stdout, "alloc %" PRIu64 ": %fs\n", alloc_count, (double)(now - starttime)/CLOCKS_PER_SEC);
-    starttime = now;
-    j = 0;
-    while (list) {
-        cu_fixed_size_memory_pool_free(pool, list->data);
-        list = cu_list_delete_link(list, list);
-/*        if (++j == alloc_count/2)
-            cu_fixed_size_memory_pool_release_empty_groups(pool, true);*/
-    }
-    now = clock();
-    fprintf(stdout, "free %" PRIu64 ": %fs\n", alloc_count, (double)(now - starttime)/CLOCKS_PER_SEC);
-    cu_fixed_size_memory_pool_alloc(pool);
 
     return 0;
 }
